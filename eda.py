e4898abaf44fd5e2e200344a909f7184486598b3
acaldwell93 <acaldwell93@gmail.com>
2021-02-25 15:48:21 -0500

    images folder

--- eda.py
+++ eda.py
@@ -1,25 +1,23 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import itertools
+import geoplot as gplt
+import geoplot.crs as gcrs
+import geopandas as gpd
+import scipy.stats as stats
+import descartes
+from shapely.geometry import Point, Polygon
 
-age_groups = ['Adult 18+', 'Teen 12-17', 'Child 0-11']
-gender = ['Male', 'Female']
-status = ['Injured', 'Killed']
-vic_or_sus = ['Victim']
+gun_df = pd.read_csv('./data/stage3.csv')
 
-all_categories = [age_groups, gender, status]
+clean_lat_long = gun_df[['n_killed', 'n_injured', 'longitude', 'latitude']].dropna()
 
-print(list(itertools.product(*all_categories)))
+geometry = [Point(xy) for xy in zip(clean_lat_long['longitude'], clean_lat_long['latitude'])]
+crs = {'init': 'epsg:4326'}
+geo_df = gpd.GeoDataFrame(clean_lat_long, crs = crs, geometry=geometry)
 
+contiguous_usa = gpd.read_file(gplt.datasets.get_path('contiguous_usa'))
+ax = gplt.polyplot(contiguous_usa, projection=gcrs.AlbersEqualArea())
+gplt.kdeplot(geo_df[geo_df['n_killed']>0], cmap='Reds', shade=True, thresh=0, clip=contiguous_usa, ax=ax)
 
-suspect_categories = [['Subject-Suspect'], age_groups, gender]
-
-print(list(itertools.product(*suspect_categories)))
-
-def add_suspect_count():
-    suspect_cats = []
-    for idx in gun_df['suspect_id']:
-        sus_age_group = gun_df['participant_age_group'][idx]
-        sus_gender = gun_df['participant_gender'][idx]
-        suspect_cats.append(f'''('Subject-Suspect', {sus_age_group}, {sus_gender})''')
+plt.show()
